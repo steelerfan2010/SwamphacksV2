6ebd771aa998e5013df8955a7442bf8e69f2c488
will <steelerfan2010@ufl.edu>
2016-04-03 19:39:10 -0400

    .

--- www/DynamicQueries.js
+++ www/DynamicQueries.js
@@ -1,11 +1,20 @@
 function getDatesForSystem(number) {
     //disable dates and other shit
-    doDatesOfSystemQuery(number, addDatesToDropdownForSystem);
+    doDatesOfSystemQuery(number, addStartDates);
+    doDatesOfSystemQuery(number, addEndDates);
 }
 
-function addDatesToDropdownForSystem(json) {
+function addStartDates(json) {
     var i;
-    var dropDown = $('#systemDates');
+    var dropDown = $('#startDates');
+    for(i = 0; i < json.length; i++) {
+        dropDown.append("<option>" + json[i].DATES + "</option>");
+    }
+}
+
+function addEndDates(json) {
+    var i;
+    var dropDown = $('#endDates');
     for(i = 0; i < json.length; i++) {
         dropDown.append("<option>" + json[i].DATES + "</option>");
     }
