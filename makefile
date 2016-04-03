672aca01e7d9bad48023d92be319747af1ba1e1f
William T. Livesey <wlivesey@stormx.cise.ufl.edu>
2016-04-03 19:52:33 -0400

    ,

--- makefile
+++ makefile
@@ -14,6 +14,7 @@ all: www/credentials.php
 	chmod 644 ~/public_html/alg.js
 	chmod 644 ~/public_html/genRandTables.js
 	chmod 644 ~/public_html/Comparison.js
+	chmod 644 ~/public_html/DynamicQueries.js
 	chmod 644 ~/public_html/MainPage.css
 	chmod 644 ~/public_html/home.css
 	chmod 644 ~/public_html/Lottery.jpg
--- www/DynamicQueries.js
+++ www/DynamicQueries.js
@@ -1,21 +1,28 @@
 function getDatesForSystem(number) {
     //disable dates and other shit
-    doDatesOfSystemQuery(number, addStartDates);
-    doDatesOfSystemQuery(number, addEndDates);
+    var a = doDatesOfSystemQuery(number, addStartDates);
+    var b = doDatesOfSystemQuery(number, addEndDates);
+    
+    $.when(a,b).then(function() {
+        $('#startDates').prop("disabled", false);
+        $('#endDates').prop("disabled", false);
+    });
 }
 
 function addStartDates(json) {
     var i;
     var dropDown = $('#startDates');
+
     for(i = 0; i < json.length; i++) {
-        dropDown.append("<option>" + json[i].DATES + "</option>");
+        dropDown.append("<option>" + json[i].DRAWINGDATE + "</option>");
     }
 }
 
 function addEndDates(json) {
     var i;
     var dropDown = $('#endDates');
+
     for(i = 0; i < json.length; i++) {
-        dropDown.append("<option>" + json[i].DATES + "</option>");
+        dropDown.append("<option>" + json[i].DRAWINGDATE + "</option>");
     }
 }
