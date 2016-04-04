b755d1a18493fb91b3f324b4c6edb0fd16259ba6
will <steelerfan2010@ufl.edu>
2016-04-03 20:34:31 -0400

    .

--- www/DynamicQueries.js
+++ www/DynamicQueries.js
@@ -34,48 +34,32 @@ function generateGraphs() {
     var endDate = $('#endDates').find(":selected").text();
 
     //should verify (start < end) HERE
-    doFrequencyInSystemQuery(startDate, endDate, 0, systemNumber, temp(function() { good("regGraph"); } ))
-    doFrequencyInSystemQuery(startDate, endDate, 1, systemNumber, temp(function() { good("powerGraph"); } ))
+    var a = doFrequencyInSystemQuery(startDate, endDate, 0, systemNumber, setGenReg);
+    var b = doFrequencyInSystemQuery(startDate, endDate, 1, systemNumber, setGenPower);
+
+    $.when(a,b).then(function() {
+        graph(regularChart, regularNumbers);
+        graph(powerballChart, powerballNumbers);
+    });
+
 }
 
-function good(json, id) {
-    console.log(json);
-    console.log(id);
+var regularNumbers;
+var powerballNumbers;
+var haveRegularNumbers = false;
+var havePowerballNumbers = false;
+
+function setGenReg(json) {
+    regularNumbers = json;
+    haveRegularNumbers = true;
 }
 
-//**********************************************************
-
-function extractRegularNumbers(json) {
-	var numbers = [];
-
-	for(var i in json) {
-		numbers.push(json[i].LOTTONUMBER);
-	}
-
-	return numbers;
+function setGenPower(json) {
+    powerballNumbers = json;
+    havePowerballNumbers = true;
 }
 
-function extractPowerballNumbers(json) {
-	var numbers = [];
-
-	for(var i in json) {
-		numbers.push(json[i].LOTTONUMBER);
-	}
-
-	return numbers;
-}
-
-function extractFrequency(json) {
-	var frequency = [];
-
-	for(var i in json) {
-		frequency.push(json[i].FREQUENCY);
-	}
-
-	return frequency;
-}
-
-function graphRegularFrequency(json) {
+function graph(id, json) {
 	var num = extractRegularNumbers(json);
 	var freq = extractFrequency(json);
 
@@ -91,100 +75,5 @@ function graphRegularFrequency(json) {
 		height: 200
 	};
 
-	new Chartist.Bar('#chart', data, options);
-}
-
-function graphPowerballFrequency(json) {
-	var num = extractPowerballNumbers(json);
-	var freq = extractFrequency(json);
-
-	var data = {
-		labels: num,
-		series: [
-			freq
-		]
-	};
-
-	var options = {
-		width: 1000,
-		height: 200
-	};
-
-	new Chartist.Bar('#chart2', data, options);
-}
-
-var regNums;
-var pbNums;
-var haveRegNums = false;
-var havePbNums = false;
-
-function setRegNums(json){
-	regNums = json;
-	haveRegNums = true;
-}
-
-function setPbNums(json){
-	pbNums = json;
-	havePbNums = true;
-}
-
-function assignNumbers() {
-    doPrediction();
-    assignRandomNumbers();
-}
-
-function doPrediction(){
-	if(haveRegNums && havePbNums){
-		var prediction = generatePrediction(regNums, pbNums, 69, 26);
-		document.getElementById("n1").innerHTML = prediction.regularPrediction[0];
-		document.getElementById("n2").innerHTML = prediction.regularPrediction[1];
-		document.getElementById("n3").innerHTML = prediction.regularPrediction[2];
-		document.getElementById("n4").innerHTML = prediction.regularPrediction[3];
-		document.getElementById("n5").innerHTML = prediction.regularPrediction[4];
-		document.getElementById("pb").innerHTML = prediction.powerballPrediction;
-	}
-}
-
-function assignRandomNumbers() {
-    var numbers = getRandomNumbers();
-    document.getElementById("rand1").innerHTML = numbers[0];
-    document.getElementById("rand2").innerHTML = numbers[1];
-    document.getElementById("rand3").innerHTML = numbers[2];
-    document.getElementById("rand4").innerHTML = numbers[3];
-    document.getElementById("rand5").innerHTML = numbers[4];
-    document.getElementById("randPb").innerHTML = numbers[5];
-}
-
-function getRandomNumbers() {
-    var numbers = [];
-    var i;
-    var numberSet = new Set();
-    while(numberSet.size < 5) {
-        numberSet.add(getRandomIntInclusive(1, 69));
-    }
-    numbers = Array.from(numberSet);
-    Array.sort(numbers);
-
-    numbers.push(getRandomIntInclusive(1, 26));
-    return numbers;
-}
-
-function getRandomIntInclusive(min, max) {
-      var number = Math.floor(Math.random() * (max - min + 1)) + min;
-      if(number < 10) {
-        number = "0" + number;
-      }
-      return number;
-}
-
-function populateMainPage(){
-	var a = doFrequencyQuery("2015-09-07", "2016-09-07", 0, setRegNums);
-	var b = doFrequencyQuery("2015-09-07", "2016-09-07", 1, setPbNums);
-
-	$.when(a,b).then(function(){
-		graphRegularFrequency(regNums);
-		graphPowerballFrequency(pbNums);
-		assignNumbers();
-	});
-
+	new Chartist.Bar('#\\id', data, options);
 }
