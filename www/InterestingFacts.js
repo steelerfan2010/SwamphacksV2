33f678b93fb07cfeef36cf1ce88ea56580a4294f
will <steelerfan2010@ufl.edu>
2016-04-05 20:26:08 -0400

    t

---
+++ www/InterestingFacts.js
@@ -0,0 +1,38 @@
+ function graphJackpotLine(json) {
+ 	var years = getYears(json);
+ 	var jackpots = getJackpotAmount(json);
+
+ 	var data = {
+ 		labels: years,
+ 		series: [
+ 			jackpots
+ 		]
+ 	};
+
+ 	var options = {
+ //		width: 1000,
+ 		height: 200
+ 	};
+
+ 	new Chartist.Line('#jackpotLineGraph', data, options);
+ }
+
+ function getYear(json) {
+    var years = [];
+
+    for(var i in json) {
+    	years.push(json[i].YEAR);
+    }
+
+    return years;
+ }
+
+ function getJackpotAmount(json) {
+    var jackpots = [];
+
+ 	for(var i in json) {
+ 		jackpots.push(json[i].JACKPOTAMOUNT);
+ 	}
+
+ 	return jackpots;
+ }
